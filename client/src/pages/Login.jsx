b52bb44bb6f110aad9ca6ea30889bfe706f77c8e
UTKARSH LADLA <128512468+utkarsh-ladla@users.noreply.github.com>
2024-09-07 20:03:16 +0530

    checkup api vercel 2.3

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -28,7 +28,7 @@ export default function LoginPage() {
     }
     
     // POST request to server
-    axios.post('https://asos-fashion-store-mern-server.vercel.app//login', formState)
+    axios.post('https://asos-fashion-store-mern-server.vercel.app/login', formState)
       .then(response => {
         if (response.data === "success") {
           // Store the login state in localStorage (or in sessionStorage)
