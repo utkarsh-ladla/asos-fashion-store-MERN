cb6ce360b0451fbad92e050cd8470ae6d783f0b8
UTKARSH LADLA <128512468+utkarsh-ladla@users.noreply.github.com>
2024-09-30 19:12:05 +0530

    server deploy 1.21

--- server/index.js
+++ server/index.js
@@ -66,11 +66,11 @@ app.post('/api/register', async (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Serve React app for all other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 // Start the server
